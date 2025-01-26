2f1680abec770fcff253506fff4e72684658c0af
Talip Tun <taliptun62@gmail.com>
2025-01-26 17:44:35 +0000

    created the data object for prayer times in a new file called PrayerTime.kt

--- app/src/main/java/com/example/prayerapp/PrayerApiService.kt
+++ app/src/main/java/com/example/prayerapp/PrayerApiService.kt
@@ -1,2 +1,8 @@
 package com.example.prayerapp
 
+import retrofit2.http.GET
+
+interface PrayerApiService {
+    @GET("https://api.aladhan.com/v1")
+    suspend fun getPrayerTimes(): List<PrayerTime>
+}
---
+++ app/src/main/java/com/example/prayerapp/PrayerTime.kt
@@ -0,0 +1,9 @@
+package com.example.prayerapp
+
+data class PrayerTime(
+    val fajr : String,
+    val dhuhr: String,
+    val asr: String,
+    val maghrib: String,
+    val isha: String
+)
