0a8c66201014ceeb3614a5a03e51f5dfdd34c40e
Talip Tun <taliptun62@gmail.com>
2025-01-26 17:29:45 +0000

    added new implementations at dependencies at build.gradle.kts(Project: PrayerApp)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,12 @@ android {
 
 dependencies {
 
+    //API start
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    // API end
+
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.9.3")
