c777a11bf06eb89dd3537df66be875c591d5dccd
Talip Tun <Talip.Tun@city.ac.uk>
2025-01-27 15:19:52 +0000

    initial commit

--- app/src/main/java/com/example/prayerapp/PrayerApiService.kt
+++ app/src/main/java/com/example/prayerapp/PrayerApiService.kt
@@ -3,6 +3,6 @@ package com.example.prayerapp
 import retrofit2.http.GET
 
 interface PrayerApiService {
-    @GET("timings/2025-01-26")
+    @GET("timings/2025-01-26/51.5194682/-0.1360365")
     suspend fun getPrayerTimes(): PrayerApiResponse
 }
