53ed08d26da2ebc4db817d3e1e1681b7a68b5707
Talip Tun <Talip.Tun@city.ac.uk>
2025-01-27 21:43:23 +0000

    test

--- app/src/main/java/com/example/prayerapp/PrayerApiService.kt
+++ app/src/main/java/com/example/prayerapp/PrayerApiService.kt
@@ -6,6 +6,6 @@ import retrofit2.http.GET
 // const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 // const val = "posts" GET /timings/27-01-2025
 interface PrayerApiService {
-    @GET("timingsByCity/01-01-2025?city=London&country=GB")
+    @GET("timingsByAddress/01-01-2025?address=Trafalgar+Square%2C+London%2C+UK&x7xapikey=P244d623e2fe2daf56359fGyK3s&method=3&shafaq=general&tune=5%2C3%2C5%2C7%2C9%2C-1%2C0%2C8%2C-6&timezonestring=UTC&calendarMethod=UAQ")
     fun getSalah(): Call<PrayerApiResponse>
 }
