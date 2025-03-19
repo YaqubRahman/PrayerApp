ea64e619a40bec048529f66d1daa451c40da3a76
YaqubRahman <YaqubRahman999@gmail.com>
2025-03-19 15:50:43 +0000

    feat: adding navbar with custom image for salah times button

--- app/src/main/java/com/example/prayerapp/MainActivity.kt
+++ app/src/main/java/com/example/prayerapp/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.layout.size
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -33,9 +34,19 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import com.example.prayerapp.ui.theme.PrayerAppTheme
@@ -48,7 +59,7 @@ class MainActivity : ComponentActivity() {
 
     fun getData() {
         val client = SalahClient().getApi().build().create(PrayerApiService::class.java)
-        client.getSalah().enqueue(object: Callback<PrayerApiResponse> {
+        client.getSalah().enqueue(object : Callback<PrayerApiResponse> {
             @RequiresApi(Build.VERSION_CODES.O)
             override fun onResponse(
                 call: Call<PrayerApiResponse>,
@@ -72,148 +83,273 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             PrayerAppTheme {
-                // Display loading/error or prayer times
-                MyScreen()
+                val navItemList = listOf(
+                    NavItem("Salah Times", ImageVector.vectorResource(id = R.drawable.homeicon)),
+                    NavItem("Notifications", Icons.Default.Notifications),
+                    NavItem("Settings", Icons.Default.Settings),
+                )
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        NavigationBar {
+                            navItemList.forEachIndexed {index, navItem ->
+                                NavigationBarItem(
+                                    selected = true,
+                                    onClick = { },
+                                    icon = {Icon(imageVector = navItem.icon, contentDescription ="Icon", Modifier.size(24.dp))},
+                                    label = {Text(text = navItem.label)}
+                                )
+                            }
+
+                        }
+                    },
+                    content = { paddingValues ->
+                        MyScreen(paddingValues) // Your screen content
+                    }
+                )
             }
         }
         getData()
     }
-}
 
 
-@Composable
-fun AnnotatedCircle(
-    modifier: Modifier = Modifier,
-    annotationAngles: List<Float> = emptyList(),
-    circleColor: Color = Color(85, 175, 85, 255),
-    dotColor: Color = Color.Blue
-) {
-    Canvas(modifier = modifier) {
-        // Get canvas dimensions
-        val centerX = size.width / 2f
-        val centerY = size.height / 2f
-        val radius = minOf(centerX, centerY)
+    @Composable
+    fun AnnotatedCircle(
+        modifier: Modifier = Modifier,
+        annotationAngles: List<Float> = emptyList(),
+        circleColor: Color = Color(85, 175, 85, 255),
+        dotColor: Color = Color.Blue
+    ) {
+        Canvas(modifier = modifier) {
+            // Get canvas dimensions
+            val centerX = size.width / 2f
+            val centerY = size.height / 2f
+            val radius = minOf(centerX, centerY)
 
-        // Draw the circle
-        drawCircle(
-            color = circleColor,
-            center = Offset(centerX, centerY),
-            radius = radius,
-            style = Stroke(width = 55f)
-        )
-
-        // Draw the annotation dots
-        annotationAngles.forEach { angle ->
-            val x = centerX + radius * cos(Math.toRadians(angle.toDouble())).toFloat()
-            val y = centerY + radius * sin(Math.toRadians(angle.toDouble())).toFloat()
-
+            // Draw the circle
             drawCircle(
-                color = dotColor,
-                radius = 10f,
-                center = Offset(x, y)
+                color = circleColor,
+                center = Offset(centerX, centerY),
+                radius = radius,
+                style = Stroke(width = 55f)
+            )
+
+            // Draw the annotation dots
+            annotationAngles.forEach { angle ->
+                val x = centerX + radius * cos(Math.toRadians(angle.toDouble())).toFloat()
+                val y = centerY + radius * sin(Math.toRadians(angle.toDouble())).toFloat()
+
+                drawCircle(
+                    color = dotColor,
+                    radius = 10f,
+                    center = Offset(x, y)
+                )
+            }
+        }
+    }
+
+
+    @Composable
+    fun MyScreen(paddingValues: PaddingValues) {
+        val backgroundColor = Color(16, 17, 17)
+        val scrollState = rememberScrollState()
+        Column(
+            modifier = Modifier
+                .background(backgroundColor)
+                .fillMaxSize()
+                .verticalScroll(scrollState)
+                .padding(50.dp, 1.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            AnnotatedCircle(modifier = Modifier.size(400.dp))
+            Spacer(modifier = Modifier.size(16.dp))
+            MyBox1()
+            Spacer(modifier = Modifier.size(16.dp))
+            MyBox2()
+            Spacer(modifier = Modifier.size(16.dp))
+            MyBox3()
+            Spacer(modifier = Modifier.size(16.dp))
+            MyBox4()
+        }
+    }
+
+    @Composable
+    fun MyBox1() {
+        val customWhite = Color(255, 122, 125, 255)
+        Box(
+            modifier = Modifier
+                .width(260.dp)
+                .height(150.dp)
+                .clip(RoundedCornerShape(30.dp))
+
+
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.londonimage),
+                contentDescription = "London Dhur Image",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(30.dp))
+            )
+
+
+            Text(
+                text = "Dhur:12:18",
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .offset(0.dp, 2.dp)
+            )
+
+            Text(
+                text = "Dhur:12:18",
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+                color = customWhite,
+                modifier = Modifier.fillMaxWidth()
             )
         }
     }
-}
 
 
-@Composable
-fun MyScreen() {
-    val backgroundColor = Color(16, 17, 17)
-    val scrollState = rememberScrollState()
-    Column(
-        modifier = Modifier
-            .background(backgroundColor)
-            .fillMaxSize()
-            .verticalScroll(scrollState)
-            .padding(50.dp, 1.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        AnnotatedCircle(modifier = Modifier.size(400.dp))
-        Spacer(modifier = Modifier.size(16.dp))
-        MyBox1()
-        Spacer(modifier = Modifier.size(16.dp))
-        MyBox2()
+    @Composable
+    fun MyBox2() {
+        val customColor = Color(27, 69, 27, 255)
+        val customWhite = Color(255, 122, 125, 255)
+        Box(
+            modifier = Modifier
+                .width(260.dp)
+                .height(150.dp)
+                .clip(RoundedCornerShape(30.dp))
+                .background(customColor)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.londonasrimage),
+                contentDescription = "London Asr Image",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(30.dp))
+            )
+
+
+
+            Text(
+                text = "Asr:14:15",
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .offset(0.dp, 2.dp)
+            )
+
+            Text(
+                text = "Asr:14:15",
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+                color = customWhite,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+
+        }
     }
-}
 
-@Composable
-fun MyBox1(){
-    val customWhite = Color(255, 122, 125, 255)
-    Box(
-        modifier = Modifier
-            .width(260.dp)
-            .height(150.dp)
-            .clip(RoundedCornerShape(30.dp))
-
-
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.londonimage),
-            contentDescription = "London Dhur Image",
+    @Composable
+    fun MyBox3() {
+        val customColor = Color(27, 69, 27, 255)
+        val customWhite = Color(255, 122, 125, 255)
+        Box(
             modifier = Modifier
-                .fillMaxSize()
-                .clip(RoundedCornerShape(30.dp)))
+                .width(260.dp)
+                .height(150.dp)
+                .clip(RoundedCornerShape(30.dp))
+                .background(customColor)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.londonmaghribimage),
+                contentDescription = "London Maghrib Image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(30.dp))
+            )
 
 
-        Text(text = "Dhur:12:18",
-            fontSize = 30.sp,
-            textAlign = TextAlign.Center,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black,
-            modifier = Modifier
-                .fillMaxWidth()
-                .offset(0.dp, 2.dp)
-        )
 
-        Text(text = "Dhur:12:18",
-            fontSize = 30.sp,
-            textAlign = TextAlign.Center,
-            color = customWhite,
-            modifier = Modifier.fillMaxWidth())
+            Text(
+                text = "Maghrib:16:43",
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .offset(0.dp, 2.dp)
+            )
+
+            Text(
+                text = "Maghrib:16:43",
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+                color = customWhite,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+
+        }
     }
-}
 
-
-@Composable
-fun MyBox2(){
-    val customColor = Color(27, 69, 27, 255)
-    val customWhite = Color(255, 122, 125, 255)
-    Box(
-        modifier = Modifier
-            .width(260.dp)
-            .height(150.dp)
-            .clip(RoundedCornerShape(30.dp))
-            .background(customColor)
-    ){
-        Image(
-            painter = painterResource(id = R.drawable.londonasrimage),
-            contentDescription = "London Asr Image",
+    @Composable
+    fun MyBox4() {
+        val customColor = Color(27, 69, 27, 255)
+        val customWhite = Color(255, 122, 125, 255)
+        Box(
             modifier = Modifier
-                .fillMaxSize()
-                .clip(RoundedCornerShape(30.dp)))
+                .width(260.dp)
+                .height(150.dp)
+                .clip(RoundedCornerShape(30.dp))
+                .background(customColor)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.londonishaimage),
+                contentDescription = "London Isha Image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(30.dp))
+            )
 
 
 
-        Text(text = "Asr:14:15",
-            fontSize = 30.sp,
-            textAlign = TextAlign.Center,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black,
-            modifier = Modifier
-                .fillMaxWidth()
-                .offset(0.dp, 2.dp)
-        )
+            Text(
+                text = "Isha:18:20",
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .offset(0.dp, 2.dp)
+            )
 
-        Text(text = "Asr:14:15",
-            fontSize = 30.sp,
-            textAlign = TextAlign.Center,
-            color = customWhite,
-            modifier = Modifier.fillMaxWidth())
+            Text(
+                text = "Isha:18:20",
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+                color = customWhite,
+                modifier = Modifier.fillMaxWidth()
+            )
 
 
-
+        }
     }
 }
--- app/src/main/java/com/example/prayerapp/PrayerApiService.kt
+++ app/src/main/java/com/example/prayerapp/PrayerApiService.kt
@@ -6,6 +6,7 @@ import retrofit2.http.GET
 // const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 // const val = "posts" GET /timings/27-01-2025
 interface PrayerApiService {
-    @GET("timings/28-01-2025?latitude=51.5194682&longitude=-0.1360365&method=3&shafaq=general&tune=5%2C3%2C5%2C7%2C9%2C-1%2C0%2C8%2C-6&timezonestring=UTC&calendarMethod=UAQ")
+    @GET("timings/{date}?latitude=51.5194682&longitude=-0.1360365&method=3&shafaq=general&tune=5%2C13%2C5%2C5%2C2%2C4%2C0%2C8%2C-6&timezonestring=Europe%2FLondon&calendarMethod=UAQ")
+    //@GET("timings/29-03-2025?latitude=51.5194682&longitude=-0.1360365&method=3&shafaq=general&tune=5%2C13%2C5%2C5%2C2%2C4%2C0%2C8%2C-6&timezonestring=UTC&calendarMethod=UAQ")
     fun getSalah(): Call<PrayerApiResponse>
 }
